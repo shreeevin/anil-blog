68818b4759fa3b6a1a6643144355a51c7c08aea7
Shreevin Inc <108927382+shreeevin@users.noreply.github.com>
2024-06-05 11:56:48 +0545

    Fallback to previous code

--- packages/blog-starter-kit/themes/hashnode/pages/[slug].tsx
+++ packages/blog-starter-kit/themes/hashnode/pages/[slug].tsx
@@ -1,4 +1,3 @@
-import { useEffect, useRef } from 'react';
 import { addArticleJsonLd } from '@starter-kit/utils/seo/addArticleJsonLd';
 import { addPublicationJsonLd } from '@starter-kit/utils/seo/addPublicationJsonLd';
 import {
@@ -8,6 +7,7 @@ import {
 import request from 'graphql-request';
 import { GetStaticPaths, GetStaticProps } from 'next';
 import Head from 'next/head';
+import { useRef } from 'react';
 import { twJoin } from 'tailwind-merge';
 import { Container } from '../components/container';
 import { AppProvider } from '../components/contexts/appContext';
@@ -47,24 +47,6 @@ const Post = ({ publication, post, morePosts }: PostProps) => {
 	const highlightJsMonokaiTheme =
 		'.hljs{display:block;overflow-x:auto;padding:.5em;background:#23241f}.hljs,.hljs-subst,.hljs-tag{color:#f8f8f2}.hljs-emphasis,.hljs-strong{color:#a8a8a2}.hljs-bullet,.hljs-link,.hljs-literal,.hljs-number,.hljs-quote,.hljs-regexp{color:#ae81ff}.hljs-code,.hljs-section,.hljs-selector-class,.hljs-title{color:#a6e22e}.hljs-strong{font-weight:700}.hljs-emphasis{font-style:italic}.hljs-attr,.hljs-keyword,.hljs-name,.hljs-selector-tag{color:#f92672}.hljs-attribute,.hljs-symbol{color:#66d9ef}.hljs-class .hljs-title,.hljs-params{color:#f8f8f2}.hljs-addition,.hljs-built_in,.hljs-builtin-name,.hljs-selector-attr,.hljs-selector-id,.hljs-selector-pseudo,.hljs-string,.hljs-template-variable,.hljs-type,.hljs-variable{color:#e6db74}.hljs-comment,.hljs-deletion,.hljs-meta{color:#75715e}.ndfHFb-c4YZDc-Wrql6b{display: none;}';
 
-	useEffect(() => {
-		const iframes = document.querySelectorAll('iframe');
-		iframes.forEach((iframe) => {
-			iframe.addEventListener('load', () => {
-				const iframeDocument = iframe.contentDocument || iframe.contentWindow?.document;
-				if (iframeDocument) {
-					const elementsToHide = iframeDocument.querySelectorAll('.ndfHFb-c4YZDc-Wrql6b');
-					elementsToHide.forEach((element) => {
-						(element as HTMLElement).style.display = 'none';
-					});
-					if (elementsToHide.length > 0) {
-						console.log(`Found and hid ${elementsToHide.length} elements with class ndfHFb-c4YZDc-Wrql6b`);
-					}
-				}
-			});
-		});
-	}, []);
-	
 	return (
 		<>
 			<Head>
